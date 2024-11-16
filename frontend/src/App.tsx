37fd397873152514078896755b382bf07864bb5e
Thomas Milburn <milburn.j.thomas@gmail.com>
2024-11-16 18:15:12 +0000

    Added basic create task page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,8 @@ import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from "./routes/home.tsx";
 import Tasks from "./routes/tasks.tsx";
-import Header from "./components/header.tsx";
 import Camera from "./routes/camera.tsx";
+import CreateTask from "./routes/createTask.tsx";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="tasks" element={<Tasks/>}/>
           <Route path="camera/:taskId" element={<Camera />}/>
+          <Route path="/create-task" element={<CreateTask/>}/>
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/routes/tasks.tsx
+++ frontend/src/routes/tasks.tsx
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import TaskCard from "../components/taskCard.tsx";
 import Task from "../types/task.ts";
+import "./tasks.css"
 
 const Tasks = () => {
+
+  const navigate = useNavigate();
 
   const [tasks, setTasks] = useState<Task[]>([]);
 
@@ -27,16 +30,25 @@ const Tasks = () => {
     ])
   }, []);
 
+  const addTask = () => {
+    navigate("/create-task")
+  }
+
   return (
     <div className="tasks">
-      <h1>Tasks</h1>
+
+      <div className="tasks__title-bar">
+        <h1>Goals</h1>
+
+        <button className="tasks__title-bar__add-task" onClick={addTask}>Add goal</button>
+      </div>
+
 
       <div className="tasks__tasks-wrapper">
         {tasks.map((task) => (
           <TaskCard key={task.id} task={task}/>
         ))}
       </div>
-
 
       <Link to={"/"}>Home</Link>
     </div>
