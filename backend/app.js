6588a54ac1832aa6158eaa96f311e0ee1382ad75
Bravewave <bravewave42@gmail.com>
2024-11-16 17:26:20 +0000

    env file fix

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const { MongoClient } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // app.use(express.static(path.join(__dirname, "public")));
+ 
+// Connect to Atlas cluster
+const client = new MongoClient(process.env.MONGO_URL);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Successfully connected to Atlas");
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
