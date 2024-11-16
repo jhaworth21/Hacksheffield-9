f41533565000cda098897f1b06497095515a85f4
Thomas Milburn <milburn.j.thomas@gmail.com>
2024-11-16 20:45:06 +0000

    Updated base url

--- backend/app.js
+++ backend/app.js
@@ -15,11 +15,11 @@ const auth_config = {
     authRequired: false,
     auth0Logout: true,
     secret: '744ac41437170dce474acb1bdc59d4379028ebabb767df49211a4ce2bc3e92f1',
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:5173',
     clientID: '93ajc3U7fnprGkkgZ5d8LPkLAfmfsNg5',
     issuerBaseURL:'https://dev-s5ynupfjsplm2b3x.us.auth0.com'
   };
-  
+
   // auth router attaches /login, /logout, and /callback routes to the baseURL
   app.use(auth(auth_config));
 
