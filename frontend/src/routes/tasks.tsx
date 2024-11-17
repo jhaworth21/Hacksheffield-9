e71853ea044dffc56c11cbc5816d358f70a1b353
jhaworth21 <83611597+jhaworth21@users.noreply.github.com>
2024-11-17 00:37:15 +0000

    added logo to the top menu bar

--- frontend/src/routes/tasks.tsx
+++ frontend/src/routes/tasks.tsx
@@ -3,6 +3,7 @@ import {useNavigate} from "react-router-dom";
 import TaskCard from "../components/taskCard.tsx";
 import Task from "../types/task.ts";
 import plusButton from "../assets/plus-icon.svg";
+import logo from "../assets/logo.png"
 import "./tasks.css"
 
 const Tasks = () => {
@@ -39,6 +40,7 @@ const Tasks = () => {
     <div className="tasks">
 
       <div className="tasks__title-bar">
+      <img src={logo} alt='Logo' className='tasks__title-bar__logo'></img>
         <h1>Make Me Study</h1>
 
         <button className="tasks__title-bar__add-task" onClick={addTask}>
