0e2bdd98170c00efbf9bde58250d062d23797a63
Chong Jia Chua <ChongJia@Chuas-MacBook-Pro.local>
2024-11-17 00:56:57 +0000

    Modifying user

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,7 +4,8 @@ const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   streakCount: { type: Number, default: 0 },
-  lastCompleted: { type: Date }
+  lastCompleted: { type: Date },
+  pending: { type: Boolean }
 }, { _id: true });
 
 const profileSchema = new mongoose.Schema({
